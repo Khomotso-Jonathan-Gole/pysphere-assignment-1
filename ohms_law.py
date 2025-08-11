72e7ffa00602bdcf398ac733e9cf381b0691d1e1
Khomotso Jonathan Gole <khomotsojonathan2@gmail.com>
2025-08-11 20:18:23 +0200

    Initial commit

---
+++ ohms_law.py
@@ -0,0 +1,12 @@
+# Author: Khomotso Jonathan Gole
+# Date: 2025-08-11
+
+# Formula: V = I * R
+
+I = float(input("Enter current (I) in amperes: "))
+
+R = float(input("Enter resistance (R) in ohms: "))
+
+V = I * R
+
+print(f"Voltage (V) = {V} volts")
